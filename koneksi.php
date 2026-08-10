b1dbb92cc456465abd1a631c335db580763d5593
Fadhilatul Azizi <azzii.zzii0311@gmail.com>
2026-08-10 10:59:52 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,20 @@
+<?php
+$conn = mysqli_connect(
+    "localhost",
+    "sampel_adminsiskapro",
+    "sampel_adminsiskapro",
+    "sampel_adminsiskapro"
+);
+
+if(!$conn){
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
+
+// Set charset to utf8mb4 for proper handling of special characters
+if (!mysqli_set_charset($conn, "utf8mb4")) {
+    die("Error setting charset: " . mysqli_error($conn));
+}
+
+// Enable error reporting for development (disable in production)
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+?>
